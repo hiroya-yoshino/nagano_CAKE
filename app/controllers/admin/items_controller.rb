a6e833a5ae93492de4bff49dab553c16e19f7702
Hiroya Yoshino <108524504+hiroya->
2022-08-06 11:20:19 +0000

    管理者画面（商品新規登録、商品詳細）の追記

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,30 +1,30 @@
 class Admin::ItemsController < ApplicationController
-  
+
   def new
     @item = Item.new
   end
-  
+
   def create
     @item = Item.new(item_params)
     if @item.save
-    redirect_to admin_item_path(params[:id])
+    redirect_to admin_item_path(@item.id)
     else
     render :new
     end
   end
-  
+
   def index
     @items = Item.all
   end
-  
+
   def show
     @item = Item.find(params[:id])
   end
-  
+
   def edit
     @item = Item.find(params[:id])
   end
-  
+
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
@@ -33,17 +33,17 @@ class Admin::ItemsController < ApplicationController
       render :edit
     end
   end
-  
+
   before_action :set_genres, only: [:new, :create, :update]
-  
+
   private
-  
+
   def set_genres
     @genres = Genre.all
   end
-  
+
   def item_params
-    params.permit(:image, :name, :introduction, :genre_id, :price, :is_active)
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
   end
-  
+
 end
