bfadaf0768ac48daa24a321b2fc8a4beed6e9b5c
Hiroya Yoshino <108524504+hiroya->
2022-08-08 11:06:07 +0000

    管理者トップページの作成

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -4,7 +4,7 @@ class Admin < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  has_many :orders, dependent: :destroy
+  
   
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,26 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
   root to: "homes#top"
-  
+
+  devise_for :customers, controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, controllers: {
+    sessions: "admin/sessions"
+  }
+
   namespace :admin do
-    resources :sessions, only: [:new, :create, :destroy]
     resources :genres, only: [:index, :create, :edit, :update]
     resources :items
     resources :customers, only: [:index, :show, :edit, :update]
     resources :homes, only: [:top]
   end
+
+
 
 end
