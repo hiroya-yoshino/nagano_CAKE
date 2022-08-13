188a0511fff21792d3d7b1421d6fe120807191a5
Hiroya Yoshino <108524504+hiroya->
2022-08-13 01:54:04 +0000

    管理者画面(商品新規登録、商品詳細、商品編集、ジャンル一覧、ジャンル編集、会員一覧)レイアウト作成完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,12 @@
 class Item < ApplicationRecord
   has_one_attached :image
-  
+
   belongs_to :genre
-  
+
   has_many :order_details, dependent: :destroy
 
   def get_image
-    self.image.variant(resize_to_fill: [100,100]).processed
+    self.image.variant(resize_to_fill: [150,200]).processed
   end
 
 end
