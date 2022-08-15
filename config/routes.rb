1e8e2b65ec183971617eac2ca5f199f183b540a0
Hiroya Yoshino <108524504+hiroya->
2022-08-15 00:18:15 +0000

    顧客画面(トップ、アバウト、新規登録)レイアウト作成中

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: "admin/homes#top"
+  root to: "public/homes#top"
+  get 'admin' => 'admin/homes#top'
+  get 'about' => 'public/homes#about'
 
-  devise_for :customers, controllers: {
+  devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
 
-  devise_for :admin, controllers: {
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
 
@@ -21,6 +23,6 @@ Rails.application.routes.draw do
     resources :order_details, only: [:update]
   end
 
-
+  resources :homes, only: [:top, :about]
 
 end
