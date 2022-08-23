6e41f0fbcdadd9c971e61db1eec7c45b7de8fe47
Hiroya Yoshino <108524504+hiroya->
2022-08-23 12:12:36 +0000

    顧客画面(商品詳細)機能とレイアウト作成完了

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,40 @@
 class Public::CartItemsController < ApplicationController
+
+  def index
+    @customer = current_customer
+    @cart_items = @customer.cart_items
+    @cart_amount = 0
+  end
+
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.item_id = cart_item_params[:item_id]
+    @cart_item.customer_id = current_customer.id
+    if @cart_item.save
+      redirect_to cart_items_path
+    else
+      render template: "public/items/show"
+    end
+  end
+
+  def destroy_all
+    current_customer.cart_item.destroy_all
+    redirect_to cart_items_path
+  end
+
+  def update
+    @cart_item = CartItem.find(params[:id])
+    if @cart_item.update(cart_item_params)
+      redirect_to cart_items_path
+    else
+      render :create
+    end
+  end
+
+  private
+
+  def cart_item_params
+      params.require(:cart_item).permit(:item_id, :amount)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,8 +6,12 @@ class Item < ApplicationRecord
   has_many :order_details, dependent: :destroy
   has_many :cart_items, dependent: :destroy
 
-  def get_image
-    self.image.variant(resize_to_fill: [150,200]).processed
+  def get_image(width,height)
+    image.variant(resize_to_limit: [width, height]).processed
+  end
+
+  def with_tax_price
+    (price * 1.1).floor
   end
 
 end
