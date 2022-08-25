6089168bdfef558f6dae33666d2406e02c2761e0
Hiroya Yoshino <108524504+hiroya->
2022-08-25 11:39:42 +0000

    顧客画面(注文情報入力)機能とレイアウト作成完了

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,15 +1,18 @@
 class Public::OrdersController < ApplicationController
-  
+
   def new
     @order = Order.new
     @customer = current_customer
+    @order.customer_id = @customer.id
     @addresses = @customer.addresses
   end
-  
+
   private
 
   def order_params
       params.require(:order).permit(:customer_id, :postal_code, :address, :name, :payment_method, :postage, :total_payment)
   end
-  
+
+  # params[:order][:selected_button]
+
 end
